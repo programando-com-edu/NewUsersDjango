7628e60b625959fdac0a8b0a7f8a7e90724aa1c1
Eduardo Augusto <105325918+programando-com-edu@users.noreply.github.com>
2023-03-14 11:48:37 -0300

    Criação das views de  Vizualização e Edição

--- app_cad_usuarios/views.py
+++ app_cad_usuarios/views.py
@@ -18,3 +18,21 @@ def usuarios(request):
     usuarios = Usuario.objects.all()
     return render(request, 'usuarios/usuarios.html', {'form': form, 'usuarios': usuarios})
 
+def view(request, pk):
+    data = {}
+    data['usuarios'] = Usuario.objects.get(pk=pk)
+    return render(request, 'usuarios/view.html', data)
+
+def edit(request, pk):
+    data = {}
+    usuario = Usuario.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = UsuarioForm(request.POST, instance=usuario)
+        if form.is_valid():
+            form.save()
+            return redirect('view', pk=pk)
+    else:
+        form = UsuarioForm(instance=usuario)
+    data['usuarios'] = usuario
+    data['form'] = form
+    return render(request, 'usuarios/edit.html', data)
--- project_cad_usuarios/urls.py
+++ project_cad_usuarios/urls.py
@@ -4,4 +4,7 @@ from app_cad_usuarios import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('usuarios/', views.usuarios, name='usuarios'),
+    path('view/<int:pk>/', views.view, name='view'),
+    path('edit/<int:pk>/', views.edit, name='edit'),
+
 ]
