105012d0aecf6fe4c63d184df78915934622d092
Eduardo Augusto <105325918+programando-com-edu@users.noreply.github.com>
2023-03-29 19:47:19 -0300

    allowed_hosts

--- project_cad_usuarios/settings.py
+++ project_cad_usuarios/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-%x=ra!x2%wy0-34e)yd_@0v@@+vaahh+8l-m75wpq^_8(o)4&k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
